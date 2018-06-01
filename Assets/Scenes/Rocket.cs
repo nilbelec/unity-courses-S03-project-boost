1e3da8e22595f706e2421304b2a6df652aca6a45
Juan Alvarez Curcho <celeblin@gmail.com>
2018-06-01 22:18:47 +0200

    S3_L45: Added rotation using A/D keys

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    new Rigidbody rigidbody;
+
 	// Use this for initialization
 	void Start () {
-		
+        rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -18,12 +20,12 @@ public class Rocket : MonoBehaviour {
     private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space)) {
-            
+            rigidbody.AddRelativeForce(Vector3.up);
         }
         if (Input.GetKey(KeyCode.A)) {
-            
+            transform.Rotate(Vector3.forward);
         } else if (Input.GetKey(KeyCode.D)) {
-            
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
