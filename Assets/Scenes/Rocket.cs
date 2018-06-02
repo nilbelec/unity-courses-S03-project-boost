f4381a11c45f539beef6d462110209ee3a8f9dc9
Juan Alvarez Curcho <celeblin@gmail.com>
2018-06-02 22:04:05 +0200

    S3_L48: Some fixes and refactorings

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -3,35 +3,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour {
+public class Rocket : MonoBehaviour
+{
 
     new Rigidbody rigidbody;
     AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        ProcessInput();
-	}
+    }
 
-    private void ProcessInput()
+    // Update is called once per frame
+    void Update()
     {
-        if (Input.GetKey(KeyCode.Space)) {
+        Thrust();
+        Rotate();
+    }
+
+    void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
             rigidbody.AddRelativeForce(Vector3.up);
             if (!audioSource.isPlaying)
                 audioSource.Play();
-        } else if (audioSource.isPlaying) {
+        }
+        else if (audioSource.isPlaying)
+        {
             audioSource.Stop();
         }
-        if (Input.GetKey(KeyCode.A)) {
+    }
+
+    void Rotate()
+    {
+        rigidbody.freezeRotation = true;
+
+        if (Input.GetKey(KeyCode.A))
+        {
             transform.Rotate(Vector3.forward);
-        } else if (Input.GetKey(KeyCode.D)) {
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidbody.freezeRotation = false;
     }
 }
