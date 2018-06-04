eb931501886c1e446e731106fb6c60117fc25157
Juan Alvarez Curcho <celeblin@gmail.com>
2018-06-04 22:37:55 +0200

    S3_L56: Added 1 second transitions between levels and death

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -10,6 +10,10 @@ public class Rocket : MonoBehaviour
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
 
+    enum State { Alive, Dying, Trascending }
+
+    State state = State.Alive;
+
     new Rigidbody rigidbody;
     AudioSource audioSource;
 
@@ -23,31 +27,49 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive){
+            Thrust();
+            Rotate();
+        } else if (audioSource.isPlaying) {
+            audioSource.Stop();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive)
+            return;
+        
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
                 break;
             case "Finish":
-                SceneManager.LoadScene(1);
+                state = State.Trascending;
+                Invoke("LoadNextLevel", 1f);
                 break;
             default:
-                SceneManager.LoadScene(0);
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f);
                 break;
         }
     }
 
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     void Thrust()
     {
-        float impulseThisFrame = mainThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
+            float impulseThisFrame = mainThrust * Time.deltaTime;
             rigidbody.AddRelativeForce(Vector3.up * impulseThisFrame);
             if (!audioSource.isPlaying)
             {
@@ -62,6 +84,8 @@ public class Rocket : MonoBehaviour
 
     void Rotate()
     {
+        if (state == State.Dying)
+            return;
         rigidbody.freezeRotation = true;
 
         float rotationThisFrame = rcsThrust * Time.deltaTime;
