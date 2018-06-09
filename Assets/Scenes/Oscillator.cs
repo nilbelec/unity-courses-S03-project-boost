e9229a06098dc7a87463f11ef1ec0d82f7cc575d
Juan Alvarez Curcho <celeblin@gmail.com>
2018-06-09 12:11:11 +0200

    S3_L60: Added oscillator script to handle moving platforms

---
+++ Assets/Scenes/Oscillator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+
+    // todo remove from inspector later
+    [Range(0f, 1f)]
+    [SerializeField]
+    float movementFactor; // 0 for not moved, 1 for fully moved
+
+    private Vector3 startingPosition;
+
+	// Use this for initialization
+	void Start () {
+        startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+	}
+}
