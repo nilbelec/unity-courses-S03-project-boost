cb6bb91cdbdfc090844d72ac71010f18cdc634c7
Juan Alvarez Curcho <celeblin@gmail.com>
2018-06-10 22:29:22 +0200

    S3_L69: Created loop level

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -100,7 +100,13 @@ public class Rocket : MonoBehaviour
 
     void LoadNextLevel()
     {
-        SceneManager.LoadScene(1);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
     void LoadFirstLevel()
